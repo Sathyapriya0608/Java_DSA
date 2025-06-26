7e9e0b62db3837a0feafcd71fe5d19eab3d5df04
sathyapriya <sathyapriyam.23csd@kongu.edu>
2025-06-26 14:47:23 +0530

    Fibonaci

--- src/Bt/BinaryTree.java
+++ src/Bt/BinaryTree.java
@@ -84,13 +84,13 @@ public class BinaryTree {
         BinaryTree  tree = new BinaryTree();
 
   
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(3);
-        tree.insert(4);
-        tree.insert(5);
-        tree.insert(6);
-        tree.insert(7);
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(40);
+        tree.insert(50);
+        tree.insert(60);
+        tree.insert(70);
 
        
         System.out.println("In-order Traversal: ");
